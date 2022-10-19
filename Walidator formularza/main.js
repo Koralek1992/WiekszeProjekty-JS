63f5b27348887ed88549094cf6db6e3b9b424642
Koralek <dkoralek2@gmail.com>
2022-10-19 13:37:02 +0200

    zrobienie hasła, porównanie haseł do siebie

--- Walidator formularza/main.js
+++ Walidator formularza/main.js
@@ -6,10 +6,51 @@ const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
-clearBtn.addEventListener('click', e => {
-    e.preventDefault();
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.error-text')
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
 
-    [username, pass, pass2, email].forEach(el => {
-        el.value = ''
-    });
-});
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+const checkLength = (input, min) => {
+    if(input.value.length < min) {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składa się z min ${min} znaków`)
+    }
+}
+
+const checkPassword = (pass1, pass2) => {
+    if(pass1.value !== pass2.value) {
+        showError(pass2, 'Hasła do siebie nie pasują')
+    }
+}
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault()
+	checkForm([username, pass, pass2, email])
+    checkLength(username, 3)
+    checkLength(pass, 8)
+    checkPassword(pass, pass2)
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault()
+	;[username, pass, pass2, email].forEach(el => {
+		el.value = ''
+	})
+})
